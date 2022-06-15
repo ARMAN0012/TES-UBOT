d53942773305074544bd4d1bf11701678baec2f1
ARMAN0012 <96458836+ARMAN0012@users.noreply.github.com>
2022-06-15 18:23:24 +0700

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -8,7 +8,7 @@
 import sys
 from importlib import import_module
 from pytgcalls import idle
-from telethon.tl.functions.channels import InviteToChannelRequest
+from pyrogram.tl.functions.channels import InviteToChannelRequest
 from userbot import BOTLOG_CHATID, BOTLOG_MSG, BOT_USERNAME, BOT_VER, LOGS, bot, karblacklist, call_py
 from userbot.modules import ALL_MODULES
 from userbot.utils.utils import autobot
