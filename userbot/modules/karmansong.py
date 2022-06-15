087282d54fbbd2225ed1eeb1c8f4dfcf011b12ef
ARMAN0012 <96458836+ARMAN0012@users.noreply.github.com>
2022-06-15 18:36:57 +0700

    Update karmansong.py

--- userbot/modules/karmansong.py
+++ userbot/modules/karmansong.py
@@ -9,8 +9,8 @@ import json
 import os
 
 import pybase64
-from telethon.tl.functions.channels import JoinChannelRequest
-from telethon.tl.types import DocumentAttributeAudio
+from pyrogram.tl.functions.channels import JoinChannelRequest
+from pyrogram.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
 from youtube_dl.utils import (
     ContentTooShortError,
