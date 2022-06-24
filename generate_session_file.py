9f5777a438854c9d69d658ee3b698ce9bfb3593b
ARMAN0012 <96458836+ARMAN0012@users.noreply.github.com>
2022-06-24 22:46:01 +0700

    Update generate_session_file.py

--- generate_session_file.py
+++ generate_session_file.py
@@ -5,7 +5,7 @@
 #
 # This script wont run your bot, it just generates a session.
 
-from pyrogram import TelegramClient
+from pyrogram import Client
 from dotenv import load_dotenv
 import os
 
@@ -14,5 +14,5 @@ load_dotenv("config.env")
 API_KEY = os.environ.get("API_KEY", None)
 API_HASH = os.environ.get("API_HASH", None)
 
-bot = TelegramClient('userbot', API_KEY, API_HASH)
+bot = Client('userbot', API_KEY, API_HASH)
 bot.start()
